9eb19b2073a0fcada81423d47a6cff3ba451df09
Pooja <poojadeurker@gmail.com>
2024-08-02 12:36:55 +0530

    first commit

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        String text = "Contains text";
+        int number = 123;
+        double pi = 3.14;
+        float num1 = 123.456f;
+
+        System.out.println("text variable : " + text);
+        System.out.println("Integer variable : " + number);
+        System.out.println("The value of pi : " + pi);
+        System.out.println("floating number : " + num1);
+    }
+}
