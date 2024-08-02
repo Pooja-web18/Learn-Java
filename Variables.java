bbe6a69c1c701b2ca1446de88518906f3c2f6aa4
Pooja <poojadeurker@gmail.com>
2024-08-02 17:04:32 +0530

    first commit

--- Variables.java
+++ Variables.java
@@ -4,10 +4,13 @@ public class Variables {
         int number = 123;
         double pi = 3.14;
         float num1 = 123.456f;
-
+        char ch = 'P';
+        long num2 = 1234567890;
         System.out.println("text variable : " + text);
         System.out.println("Integer variable : " + number);
         System.out.println("The value of pi : " + pi);
         System.out.println("floating number : " + num1);
+        System.out.println("Character  : " + ch);
+        System.out.println("long number : " + num2);
     }
 }
